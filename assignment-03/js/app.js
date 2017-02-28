7a21854593d87a41cf93a80c1e878f77a1b4d17b
IsaiahGOW <isaiah.serrano@f1006-13-34173.bmcc.cuny.edu>
2017-02-27 19:06:17 -0500

    js added
    added var function, alerts, else and if , console

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -1,2 +1,34 @@
 /*jslint browser, es6, single, for, devel, this */
 /*global window */
+
+var enlargeButton = document.getElementById('enlarge-div');
+
+var shrinkButton = document.getElementById('shrink-div');
+
+function showEnlargeAlert() {
+'use strict';
+    alert('enlarge-div');
+}
+
+function showShrinkAlert() {
+'use strict';
+    alert('shrink-div');
+}
+
+//function showAlert() {
+//'use strict';
+//    alert('clicked');
+//}
+
+if(null !== enlargeButton ){
+enlargeButton.addEventListener('click', showEnlargeAlert, false); 
+    
+} else { 
+    console.error('No reference to ID enlarge-div established');
+}
+
+if(null !== shrinkButton ){
+shrinkButton.addEventListener('click', showShrinkAlert, false);
+} else { 
+    console.error('No reference to ID shrink-div established');
+}
